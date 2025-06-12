c50e0d081e44462eac54e134095f553328910c92
dannnnnm <137915605+dannnnnm@users.noreply.github.com>
2025-06-12 00:37:15 -0400

    nombre apropiado para archivos de test y comentar test de ejemplo porque la app ya no es asi

---
+++ test/customTypesTest/option_test.dart
@@ -0,0 +1,27 @@
+import 'package:bitacora_ejercicios/utilTypes/option.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test("Some returns true to isSome", () {
+    Option<int> option = Some(42);
+    expect(option.isSome(), true);
+  });
+  test("Some returns false to isNone", () {
+    Option<int> option = Some(42);
+    expect(option.isNone(), false);
+  });
+  test("None returns false to isSome", () {
+    None option = None();
+    expect(option.isSome(), false);
+  });
+  test("Nonee returns true to isNone", () {
+    None option = None();
+    expect(option.isNone(), true);
+  });
+
+  test("Some returns the contained value", () {
+    String value = "is that the door?";
+    Option<String> option = Some(value);
+    expect(option.unwrap(), value);
+  });
+}

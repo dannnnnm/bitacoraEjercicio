f2be871738864035a1ee56b113fb301d47efa84a
Daniel <d.aguayo02@ufromail.cl>
2025-06-13 10:18:31 -0400

    corregir errores en persistencia y carga de modelo

--- lib/model/evidence_image.dart
+++ lib/model/evidence_image.dart
@@ -5,11 +5,11 @@ import 'package:bitacora_ejercicios/utilTypes/json_map.dart';
 import 'package:flutter/material.dart';
 
 class EvidenceImage {
-  EvidenceImage(this.bytes) : image = Image.memory(bytes), activityID = 0;
-  EvidenceImage.load(int id, this.activityID, Uint8List bytes)
+  EvidenceImage(this.bytes) : image = Image.memory(bytes), activityID = 0, id=0;
+  EvidenceImage.load(this.id, this.activityID, this.bytes)
     : image = Image.memory(bytes);
-  int id = 0;
-  Uint8List bytes = Uint8List(0);
+  int id;
+  Uint8List bytes;
   Image image;
   int activityID;
 
@@ -18,7 +18,7 @@ class EvidenceImage {
     if (id != 0) {
       json["id"] = id;
     }
-    if (activityID != 0) {
+    if (activityID == 0) {
       throw UnregisteredComponentException(runtimeType.toString(), "activity");
     }
     return json;

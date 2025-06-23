34b1208e3537871297e68f26bf71cc86a9ebc2c5
dannnnnm <137915605+dannnnnm@users.noreply.github.com>
2025-06-23 17:03:55 -0400

    test de integracion guardando actividades

--- integration_test/activity_crud_test.dart
+++ integration_test/activity_crud_test.dart
@@ -1,6 +1,7 @@
-
 import 'package:bitacora_ejercicios/main.dart';
 import 'package:bitacora_ejercicios/main.dart' as app;
+import 'package:bitacora_ejercicios/model/category.dart';
+import 'package:bitacora_ejercicios/repository/category_repository.dart';
 import 'package:bitacora_ejercicios/screen/add_activity.dart';
 import 'package:bitacora_ejercicios/screen/main_screen.dart';
 import 'package:convenient_test_dev/convenient_test_dev.dart';
@@ -8,29 +9,78 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get/get.dart';
 
-void main(){
-  convenientTestMain(BitacoraConvenientTestSlot(), (){
+void main() {
+  convenientTestMain(BitacoraConvenientTestSlot(), () {
     tTestWidgets("Agregar actividad funciona", (t) async {
-      var actName="test ${DateTime.now().millisecondsSinceEpoch}";
+      await CategoryRepository().saveOne(Category("testCat"));
+      await t.tester.pumpAndSettle(Duration(seconds: 2));
+      var actName = "test ${DateTime.now().millisecondsSinceEpoch}";
+      await t.tester.ensureVisible(find.byKey(newActivityFABKey));
+      await t.get(find.byKey(newActivityFABKey)).tap();
+      await t.tester.pumpAndSettle(Duration(seconds: 2));
+      await t.tester.ensureVisible(find.byKey(newActivityNameKey));
+      await t
+          .get(find.byKey(newActivityNameKey))
+          .enterTextWithoutReplace(actName);
+      await t
+          .get(find.byKey(newActivityDescriptionKey))
+          .enterTextWithoutReplace("some description");
+      await t
+          .get(find.byKey(newActivityScoreKey))
+          .enterTextWithoutReplace("2.3");
 
-      t.get(find.byKey(Key(newActivityFAB))).tap();
-      t.get(find.byKey(newActivityNameKey)).enterTextWithoutReplace(actName);
-      t.get(find.byKey(newActivityDescriptionKey)).enterTextWithoutReplace("some description");
-      t.get(find.byKey(newActivityScoreKey)).enterTextWithoutReplace("2.3");
-      t.get(find.byKey(addActivityFABKey)).tap();
-      find.text(actName);
+      await t.get(find.byKey(addActivityFABKey)).tap();
+      await t.tester.pumpAndSettle(Duration(seconds: 2));
+      await t.tester.ensureVisible(find.byKey(Key("${actList}0")));
+      expect(find.text(actName), findsOneWidget);
+    });
+
+    tTestWidgets("Agregar actividad funciona creando nueva categoria", (
+      t,
+    ) async {
+      await t.tester.pumpAndSettle(Duration(seconds: 2));
+      var catName = "cat ${DateTime.now().millisecondsSinceEpoch}";
+      var actName = "test ${DateTime.now().millisecondsSinceEpoch}";
+      await t.tester.ensureVisible(find.byKey(newActivityFABKey));
+      await t.get(find.byKey(newActivityFABKey)).tap();
+      await t.tester.pumpAndSettle(Duration(seconds: 2));
+      await t.tester.ensureVisible(find.byKey(newActivityNameKey));
+      await t
+          .get(find.byKey(newActivityNameKey))
+          .enterTextWithoutReplace(actName);
+      await t
+          .get(find.byKey(newActivityDescriptionKey))
+          .enterTextWithoutReplace("some description");
+
+      await t.get(find.byKey(newCategoryButtonKey)).tap();
+      await t
+          .get(find.byKey(newCategoryNameKey))
+          .enterTextWithoutReplace(catName);
+
+      await t.get(find.byKey(saveNewCategoryButton)).tap();
+
+      await t
+          .get(find.byKey(newActivityScoreKey))
+          .enterTextWithoutReplace("2.3");
+
+      await t.get(find.byKey(addActivityFABKey)).tap();
+      await t.tester.pumpAndSettle(Duration(seconds: 2));
+      await t.tester.ensureVisible(find.byKey(Key("${actList}0")));
+      await find.text(actName).tap();
+      await t.tester.pumpAndSettle(Duration(seconds: 2));
+      //await t.tester.ensureVisible(find.byKey(newActivityNameKey));
+
+      expect(find.text(catName), findsOneWidget);
     });
   });
 }
-
 
 class BitacoraConvenientTestSlot extends ConvenientTestSlot {
   @override
   Future<void> appMain(AppMainExecuteMode mode) async => app.main();
 
   @override
-  BuildContext? getNavContext(ConvenientTest t) =>
-      Get.context;
+  BuildContext? getNavContext(ConvenientTest t) => Get.context;
 }
 
 extension on ConvenientTest {
